5f915d1e1e43e67a26e293d0dce2d045cea4dd35
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-06-28 23:11:24 +0500

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import ThemeProvider from "../context/ThemeContext.js";
 import ContextDemo from "./ContextDemo";
 
 function App() {
+  const [heading, setHeading] = useState("REACT CONTEXT API");
   return (
     <div className='App'>
+      <h1>{heading}</h1>
       <ThemeProvider>
         <ContextDemo />
       </ThemeProvider>

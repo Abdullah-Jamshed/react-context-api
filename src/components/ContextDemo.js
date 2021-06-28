203f43c79f7d11ec1dd86c35ec8550b45a0c9234
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-06-28 23:02:51 +0500

    context api

--- src/components/ContextDemo.js
+++ src/components/ContextDemo.js
@@ -5,10 +5,12 @@ import Text from "./Text";
 const ContextDemo = () => {
   const { theme, setTheme } = useContext(ThemeContext);
 
+  const toggle = () => setTheme(theme === "light" ? "dark" : "light");
+
   return (
     <div style={{ backgroundColor: `${theme === "light" ? "#fff" : "#000"} ` }} className='bg'>
       <Text>{theme}</Text>
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")} className='btn'>
+      <button onClick={toggle} className='btn'>
         toggle theme
       </button>
     </div>
